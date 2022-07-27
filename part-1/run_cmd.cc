4e70516ad9a671509918c735096f05c05c00756c
Cesar Gutierrez <cesarg7@csu.fullerton.edu>
2022-07-26 19:01:15 -0700

    Pushing current progress.

--- part-1/run_cmd.cc
+++ part-1/run_cmd.cc
@@ -1,3 +1,13 @@
+// Cesar Gutierrez
+// CPSC 353-02
+// 2022-07-26
+// cesarg7@csu.fullerton.edu
+// @cesar-gz
+//
+// Lab 02-01
+//
+// This is an exercise in detecting buffer overflow.
+//
 
 #include <cstdlib>
 #include <iostream>
@@ -11,7 +21,16 @@ using namespace std;
 /// \param s The string to repeat, must be 1 byte or larger.
 /// \returns the string \p s repeated \p n_repeat times
 string BigString(int n_repeat, const string& s) {
-  return string("foo");
+  string newString = "";
+
+  for (int i = 0; i < n_repeat; i++) {
+    newString = newString + s;
+    if (int(newString.size()) >= n_repeat) {
+      break;
+    }
+  }
+
+  return string(newString);
 }
 
 /// Entry point to the test harness which repeated executes the
@@ -28,7 +47,7 @@ int main(int argc, char const* argv[]) {
   string base_cmd = string(argv[1]);
   string base_str = string(argv[2]);
   int rv = 0;
-  int n_repeat = 0;
+  int n_repeat = base_str.size();
   string repeated = BigString(n_repeat, base_str);
   // Concatenate the command and the repeated string as a
   // single string. Redirect all output to /dev/null to run
